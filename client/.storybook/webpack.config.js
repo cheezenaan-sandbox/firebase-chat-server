730e4232c6dfa7dc3512bb95e1add47a72c688f8
cheezenaan <cheezenaan@gmail.com>
2018-06-19 08:49:45 +0900

    Introduce file-loader and url-loader

--- client/.storybook/webpack.config.js
+++ client/.storybook/webpack.config.js
@@ -13,6 +13,15 @@ module.exports = {
         include: /node_modules/,
         loader: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+          },
+        },
+      },
     ],
   },
 };

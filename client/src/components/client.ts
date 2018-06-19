b1417c36e47531d27a3173b261c50eff760b2fdf
cheezenaan <cheezenaan@gmail.com>
2018-06-19 09:11:22 +0900

    Implement HTTP client logic

---
+++ client/src/components/client.ts
@@ -0,0 +1,17 @@
+import Axios, { AxiosInstance, AxiosResponse, CancelToken } from 'axios';
+
+import { Message } from '../../../custom';
+
+const baseURL = 'https://getting-started-frontend-4545.firebaseapp.com/api/v1';
+
+const instance: AxiosInstance = Axios.create({
+  baseURL,
+  timeout: 10000,
+});
+
+export const fetchMessages = (
+  channelName: string,
+  params = {},
+  cancelToken: CancelToken | undefined = undefined // TODO: undefined でいいのか…?
+): Promise<AxiosResponse<{ messages: Message[] }>> =>
+  instance.get(`/channels/${channelName}/messages`, { params, cancelToken });

79acca20de0b9e437c08e49aa79acd00e080da34
cheezenaan <cheezenaan@gmail.com>
2018-07-14 19:35:48 +0900

    /channels/ 以下に直アクセスした際の rewrite 設定を追加
    * dev(webpack-serve) では connect-history-api-fallback を使用
    * production では Firebase Hosting の rewrites を使用

--- client/config/serve.config.js
+++ client/config/serve.config.js
@@ -1,4 +1,6 @@
 const config = require('./webpack.config');
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
 
 module.exports = {
   ...config,
@@ -10,6 +12,10 @@ module.exports = {
         server: '0.0.0.0',
         client: process.env.LOCAL_IP || '0.0.0.0',
       },
+    },
+    add: (app, middleware, options) => {
+      const historyOptions = { index: '/index.html' };
+      app.use(convert(history(historyOptions)));
     },
   },
 };
